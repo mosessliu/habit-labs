037cbda7679827f01955f92a665eee9e095111a2
Moses Liu <moses.s.liu@gmail.com>
2018-05-30 23:10:15 -0700

    refactored habit model

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -13,27 +13,18 @@ class Habit < ApplicationRecord
 
   def get_time_left
     curr = DateTime.current
-    next_deadline = next_deadline()
-
-    #next_deadline should not be nil because habit should have been sweeped already
-
+    next_deadline = next_deadline() #next_deadline should not be nil because habit should have been sweeped already
     diff = next_deadline - curr
-    if self.frequency == 0
-      days_left = diff / 1.days
-      if days_left < 1 
-        return pluralize((diff / 1.hours).floor, "hour")
-      end
-      return pluralize(days_left.floor, "day")
+
+    if is_daily?
+      return days_or_hours_left(diff)
     else
       weeks_left = diff / 1.weeks
       if weeks_left < 1
-        days_left = diff / 1.days
-        if days_left < 1 
-          return pluralize((diff / 1.hours).floor, "hour")
-        end
-        return pluralize(days_left.floor, "day")
+        return days_or_hours_left(diff)
+      else
+        return pluralize(weeks_left, "week")
       end
-      return pluralize(weeks_left.floor, "week")
     end
   end 
 
@@ -53,6 +44,27 @@ class Habit < ApplicationRecord
     user_habit = UserHabit.where(user: user, habit: self).first
     user_habit_deadline = UserHabitDeadline.where(user_habit: user_habit, deadline: next_deadline).first
     return user_habit_deadline.completed
+  end
+
+  def is_recurring?
+    return duration > 0
+  end
+
+  def is_daily?
+    return frequency == 0
+  end
+
+  def is_weekly?
+    return frequency == 1
+  end
+
+  private 
+  def days_or_hours_left(time)
+    days_left = time / 1.days
+    if days_left < 1 
+      return pluralize((time / 1.hour).floor, "hour")
+    end
+    return pluralize(days_left.floor, "day")
   end
 
 end
